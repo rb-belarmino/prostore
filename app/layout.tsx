24ed653a3b523d18bc9fac848df5cd921d317928
Rodrigo Belarmino <rb.belarmino@gmail.com>
2024-12-27 16:13:28 -0300

    feat: add theme toggle functionality with dropdown menu and integrate next-themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/assets/styles/globals.css";
 import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants";
+import { ThemeProvider } from "next-themes";
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -20,11 +21,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" suppressHydrationWarning>
       <body
         className={`${inter.className} antialiased`}
       >
-        {children}
+        <ThemeProvider attribute="class" defaultTheme="light" disableTransitionOnChange>
+            {children}
+        </ThemeProvider>
       </body>
     </html>
   );
