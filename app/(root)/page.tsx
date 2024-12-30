839f7492cd6ff128ec288c4eb0dd4234fbd6578d
Rodrigo Belarmino <rb.belarmino@gmail.com>
2024-12-30 18:43:03 -0300

    feat: add product listing components and sample data for e-commerce functionality

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,12 @@
+import ProductList from "@/components/shared/products/product-list";
+import sampleData from "@/db/sample-data";
+
 const HomePage = () => {
-  
-  return ( <>ProStore</> );
+  console.log(sampleData);
+  return ( 
+    <>
+      <ProductList data={sampleData.products} title="Newest Arrivals" limit={4}/>
+    </> );
 }
  
 export default HomePage;
