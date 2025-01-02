1fda2f5d1ce955cfb3540c12d78006c8f95724b5
Rodrigo Belarmino <rb.belarmino@gmail.com>
2025-01-02 16:29:09 -0300

    feat: integrate Neon database with Prisma and implement product details page

---
+++ app/(root)/product/[slug]/page.tsx
@@ -0,0 +1,15 @@
+import { getProductBySlug } from '@/lib/actions/product.actions'
+import { notFound } from 'next/navigation'
+
+const ProductDetailsPage = async (props: {
+  params: Promise<{ slug: string }>
+}) => {
+  const { slug } = await props.params
+
+  const product = await getProductBySlug(slug)
+  if (!product) notFound()
+
+  return <>{product.name}</>
+}
+
+export default ProductDetailsPage
--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,14 +1,12 @@
 'use server'
 
-import { PrismaClient } from '@prisma/client'
+import { prisma } from '@/db/prisma'
 import { convertToPlainObject } from '../utils'
 import { LATEST_PRODUCTS_LIMIT } from '../constants'
 
 // Get latest products
 
 export async function getLatestProducts() {
-  const prisma = new PrismaClient()
-
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: {
@@ -17,4 +15,14 @@ export async function getLatestProducts() {
   })
 
   return convertToPlainObject(data)
+}
+
+// Get single product by slug
+
+export async function getProductBySlug(slug: string) {
+  return await prisma.product.findFirst({
+    where: {
+      slug: slug
+    }
+  })
 }
