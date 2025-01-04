0c5f4f0c33fc3f9550fd9549a0193d51f7ac14b7
Rodrigo Belarmino <rb.belarmino@gmail.com>
2025-01-04 12:04:22 -0300

    fix: update Product type to change rating to string and add numReviews

--- types/index.ts
+++ types/index.ts
@@ -3,6 +3,7 @@ import { insertProductSchema } from '@/lib/validators'
 
 export type Product = z.infer<typeof insertProductSchema> & {
   id: string
-  rating: number
+  rating: string
+  numReviews: number
   createdAt: Date
 }
