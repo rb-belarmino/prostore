a9cdd56023593701466d90a3a5e09a6f3c92cdfe
Rodrigo Belarmino <rb.belarmino@gmail.com>
2025-01-05 21:07:12 -0300

    feat: implement sign-in form validation and user authentication actions

--- lib/validators.ts
+++ lib/validators.ts
@@ -24,4 +24,7 @@ export const insertProductSchema = z.object({
 
 // Schema for signing users in
 
-export const signInFormSchema = z.object({})
+export const signInFormSchema = z.object({
+  email: z.string().email('Invalid email address'),
+  password: z.string().min(6, 'Password must be at least 6 characters')
+})
