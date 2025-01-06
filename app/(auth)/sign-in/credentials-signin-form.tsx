609e0bc30b74d05fe2ad3fb38d1b36bb9f383bac
Rodrigo Belarmino <rb.belarmino@gmail.com>
2025-01-06 13:01:21 -0300

    feat: add credentials sign-in form and related components

---
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { signInDefaultValues } from '@/lib/constants'
+import Link from 'next/link'
+
+const CredentialsSignInForm = () => {
+  return (
+    <form>
+      <div className="space-y-6">
+        <div>
+          <Label htmlFor="email">Email</Label>
+          <Input
+            id="email"
+            name="email"
+            type="email"
+            required
+            autoComplete="email"
+            defaultValue={signInDefaultValues.email}
+          />
+        </div>
+        <div>
+          <Label htmlFor="email">Password</Label>
+          <Input
+            id="password"
+            name="password"
+            type="password"
+            required
+            autoComplete="password"
+            defaultValue={signInDefaultValues.password}
+          />
+        </div>
+
+        <Button className="w-full" variant="default">
+          Sign In
+        </Button>
+      </div>
+      <div className="text-sm text-center text-muted-foreground">
+        Don&apos;t have an account?{' '}
+        <Link href="/sign-up" className="link" target="_blank">
+          Sign-Up
+        </Link>
+      </div>
+    </form>
+  )
+}
+
+export default CredentialsSignInForm
