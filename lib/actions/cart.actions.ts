b59cd13d7dbcbbc86e16cf1e5825c42ffda3395d
Rodrigo Belarmino <rb.belarmino@gmail.com>
2025-01-07 16:21:58 -0300

    feat: implement remove item from cart functionality with quantity handling

--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -146,3 +146,58 @@ export async function getMyCart() {
     totalPrice: cart.totalPrice.toString()
   })
 }
+
+export async function removeItemFromCart(productId: string) {
+  try {
+    // Check for cart cookie
+    const sessionCartId = (await cookies()).get('sessionCartId')?.value
+    if (!sessionCartId) throw new Error('Cart session not found')
+
+    // Get Product
+    const product = await prisma.product.findFirst({
+      where: { id: productId }
+    })
+
+    if (!product) throw new Error('Product not found')
+
+    // Get User Cart
+    const cart = await getMyCart()
+    if (!cart) throw new Error('Cart not found')
+
+    //Check for item
+    const exist = (cart.items as CartItem[]).find(
+      x => x.productId === productId
+    )
+    if (!exist) throw new Error('Item not found in cart')
+
+    // Check if only one in qty
+    if (exist.qty === 1) {
+      // Remove item from cart
+      cart.items = (cart.items as CartItem[]).filter(
+        x => x.productId !== exist.productId
+      )
+    } else {
+      // Decrease the quantity
+      ;(cart.items as CartItem[]).find(x => x.productId === productId)!.qty =
+        exist.qty - 1
+    }
+
+    // Update cart in databse
+    await prisma.cart.update({
+      where: { id: cart.id },
+      data: {
+        items: cart.items as Prisma.CartUpdateitemsInput[],
+        ...calcPrice(cart.items as CartItem[])
+      }
+    })
+
+    revalidatePath(`/product/${product.slug}`)
+
+    return {
+      success: true,
+      message: `${product.name} removed from cart`
+    }
+  } catch (error) {
+    return { success: false, message: formatError(error) }
+  }
+}
