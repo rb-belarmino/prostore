78dc6e339d0458558f6dd4c781afea2e2317a860
Rodrigo Belarmino <rb.belarmino@gmail.com>
2025-01-07 20:42:58 -0300

    feat: implement cart item removal and update functionality in AddToCart component

--- components/shared/products/add-to-cart.tsx
+++ components/shared/products/add-to-cart.tsx
@@ -2,13 +2,13 @@
 
 import { Button } from '@/components/ui/button'
 import { useRouter } from 'next/navigation'
-import { Plus } from 'lucide-react'
-import { CartItem } from '@/types'
+import { Plus, Minus } from 'lucide-react'
+import { Cart, CartItem } from '@/types'
 import { useToast } from '@/hooks/use-toast'
 import { ToastAction } from '@/components/ui/toast'
-import { addItemToCart } from '@/lib/actions/cart.actions'
+import { addItemToCart, removeItemFromCart } from '@/lib/actions/cart.actions'
 
-const AddToCart = ({ item }: { item: CartItem }) => {
+const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   const router = useRouter()
   const { toast } = useToast()
 
@@ -38,7 +38,34 @@ const AddToCart = ({ item }: { item: CartItem }) => {
     })
   }
 
-  return (
+  // Handke remove from cart
+
+  const handleRemoveFromCart = async () => {
+    const res = await removeItemFromCart(item.productId)
+
+    toast({
+      variant: res.success ? 'default' : 'destructive',
+      description: res.message
+    })
+
+    return
+  }
+
+  // Check if item is in cart
+
+  const existItem = cart && cart.items.find(x => x.productId === item.productId)
+
+  return existItem ? (
+    <div>
+      <Button type="button" variant="outline" onClick={handleRemoveFromCart}>
+        <Minus className="h-4 w-4" />
+      </Button>
+      <span className="px-2">{existItem.qty}</span>
+      <Button type="button" variant="outline" onClick={handleAddToCart}>
+        <Plus className="h-4 w-4" />
+      </Button>
+    </div>
+  ) : (
     <Button className="w-full" type="button" onClick={handleAddToCart}>
       <Plus /> Add To Cart
     </Button>
