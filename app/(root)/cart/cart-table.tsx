7548dba5e082b15e7ddae9a5c106ba9ac4970ca4
Rodrigo Belarmino <rb.belarmino@gmail.com>
2025-01-14 20:00:45 -0300

    feat: add subtotal display and checkout button to cart table component

--- app/(root)/cart/cart-table.tsx
+++ app/(root)/cart/cart-table.tsx
@@ -17,6 +17,8 @@ import {
   TableCell
 } from '@/components/ui/table'
 import { Button } from '@/components/ui/button'
+import { Card, CardContent } from '@/components/ui/card'
+import { formatCurrency } from '@/lib/utils'
 
 const CartTable = ({ cart }: { cart?: Cart }) => {
   const router = useRouter()
@@ -114,6 +116,31 @@ const CartTable = ({ cart }: { cart?: Cart }) => {
               </TableBody>
             </Table>
           </div>
+
+          <Card>
+            <CardContent className="p-4 gap-4">
+              <div className="p-3 text-xl">
+                Subtotal ({cart.items.reduce((a, c) => a + c.qty, 0)}):
+                <span className="font-bold">
+                  {formatCurrency(cart.itemsPrice)}
+                </span>
+              </div>
+              <Button
+                onClick={() =>
+                  startTransition(() => router.push('/shipping-address'))
+                }
+                className="w-full"
+                disabled={isPending}
+              >
+                {isPending ? (
+                  <Loader className="w-4 h-4 animate-spin" />
+                ) : (
+                  <ArrowRight className="w-4 h-4" />
+                )}
+                Proceed to Checkout
+              </Button>
+            </CardContent>
+          </Card>
         </div>
       )}
     </>
--- lib/utils.ts
+++ lib/utils.ts
@@ -57,3 +57,21 @@ export function round2(value: number | string): number {
     throw new Error('Value is not number or string')
   }
 }
+
+const CURRENCY_FORMATTER = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2
+})
+
+// Format currency using formatter above
+
+export function formatCurrency(amount: number | string | null) {
+  if (typeof amount === 'number') {
+    return CURRENCY_FORMATTER.format(amount)
+  } else if (typeof amount === 'string') {
+    return CURRENCY_FORMATTER.format(Number(amount))
+  } else {
+    return 'NaN'
+  }
+}
