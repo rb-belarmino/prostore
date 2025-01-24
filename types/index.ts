c61b1ef402466f6a17b7aedbe3a1f1038a48dfef
Rodrigo Belarmino <rb.belarmino@gmail.com>
2025-01-24 18:15:28 -0300

    feat: add validation schemas for order and order item insertion

--- types/index.ts
+++ types/index.ts
@@ -3,7 +3,9 @@ import {
   insertProductSchema,
   insertCartSchema,
   cartItemSchema,
-  shippingAddressSchema
+  shippingAddressSchema,
+  insertOrderSchema,
+  insertOrderItemSchema
 } from '@/lib/validators'
 
 export type Product = z.infer<typeof insertProductSchema> & {
@@ -16,3 +18,17 @@ export type Product = z.infer<typeof insertProductSchema> & {
 export type Cart = z.infer<typeof insertCartSchema>
 export type CartItem = z.infer<typeof cartItemSchema>
 export type ShippingAddress = z.infer<typeof shippingAddressSchema>
+export type Order = z.infer<typeof insertOrderSchema> & {
+  id: string
+  isPaid: boolean
+  paidAt: Date | null
+  isDelivered: boolean
+  deliveredAt: Date | null
+  orderItems: OrderItem[]
+  user: {
+    name: string
+    email: string
+  }
+  createdAt: Date
+}
+export type OrderItem = z.infer<typeof insertOrderItemSchema>
