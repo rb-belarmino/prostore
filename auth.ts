84095d0b227d03291d1293cb0e0eca4b6d91dc25
Rodrigo Belarmino <rb.belarmino@gmail.com>
2025-02-21 18:23:51 -0300

    feat: implement profile update functionality with session handling and adjust pagination size

--- auth.ts
+++ auth.ts
@@ -108,6 +108,13 @@ export const config = {
           }
         }
       }
+
+      // Handle session updates
+
+      if (session?.user.name && trigger === 'update') {
+        token.name = session.user.name
+      }
+
       return token
     }
   }
