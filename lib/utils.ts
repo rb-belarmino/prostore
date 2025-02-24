77c44421c9febfd868a1850209b56b843b11197b
Rodrigo Belarmino <rb.belarmino@gmail.com>
2025-02-24 18:53:42 -0300

    feat: implement admin overview page with order summary, sales, and customer metrics

--- lib/utils.ts
+++ lib/utils.ts
@@ -77,6 +77,13 @@ export function formatCurrency(amount: number | string | null) {
   }
 }
 
+// Format Number
+
+const NUMBER_FORMATTER = new Intl.NumberFormat('en-US')
+
+export function formatNumber(number: number) {
+  return NUMBER_FORMATTER.format(number)
+}
 // Shortehn UUID
 
 export function formatId(id: string) {
