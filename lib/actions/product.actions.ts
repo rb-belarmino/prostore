75892fb2a4f244b0392c44d46fc416a4476219c0
Rodrigo Belarmino <rb.belarmino@gmail.com>
2025-02-27 16:32:06 -0300

    feat: add functionality to retrieve a single product by ID and create an admin update page for products

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -30,6 +30,18 @@ export async function getProductBySlug(slug: string) {
   })
 }
 
+// Get single product by ID
+
+export async function getProductById(productId: string) {
+  const data = await prisma.product.findFirst({
+    where: {
+      id: productId
+    }
+  })
+
+  return convertToPlainObject(data)
+}
+
 // Get all products
 
 export async function getAllProducts({
