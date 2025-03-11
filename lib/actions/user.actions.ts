033e5f9283ecbc9ee0dfc27fea12e8fbb8c9af96
Rodrigo Belarmino <rb.belarmino@gmail.com>
2025-03-11 17:27:53 -0300

    feat: add admin user management page with pagination and user retrieval functionality

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -13,6 +13,7 @@ import { prisma } from '@/db/prisma'
 import { formatError } from '../utils'
 import { ShippingAddress } from '@/types'
 import { z } from 'zod'
+import { PAGE_SIZE } from '../constants'
 
 // Sign in the user with credentials
 export async function signInWithCredentials(
@@ -180,5 +181,30 @@ export async function updateProfile(user: { name: string; email: string }) {
     return { success: true, message: 'User updated successfully' }
   } catch (error) {
     return { success: false, message: formatError(error) }
+  }
+}
+
+// Get all the users
+
+export async function getAllUsers({
+  limit = PAGE_SIZE,
+  page
+}: {
+  limit?: number
+  page: number
+}) {
+  const data = await prisma.user.findMany({
+    orderBy: {
+      createdAt: 'desc'
+    },
+    take: limit,
+    skip: (page - 1) * limit
+  })
+
+  const dataCount = await prisma.user.count()
+
+  return {
+    data,
+    totalPages: Math.ceil(dataCount / limit)
   }
 }
