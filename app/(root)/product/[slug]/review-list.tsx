7ab527885e88a5f7e257b36f27c141c679c251e8
Rodrigo Belarmino <rb.belarmino@gmail.com>
2025-03-14 15:46:35 -0300

    feat: add review form component and integrate it into the review list

--- app/(root)/product/[slug]/review-list.tsx
+++ app/(root)/product/[slug]/review-list.tsx
@@ -2,6 +2,7 @@
 import { Review } from '@/types'
 import Link from 'next/link'
 import { useState } from 'react'
+import ReviewForm from './review-form'
 
 const ReviewList = ({
   userId,
@@ -17,7 +18,7 @@ const ReviewList = ({
     <div className="space-y-4">
       {reviews.length === 0 && <div>No reviews yet</div>}
       {userId ? (
-        <>{/* REVIEW FORM HERE */}</>
+        <ReviewForm userId={userId} productId={productId} />
       ) : (
         <div>
           Please
