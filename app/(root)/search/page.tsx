733b007dc8da46f582be46e90d73b39486ddec33
Rodrigo Belarmino <rb.belarmino@gmail.com>
2025-03-13 22:52:14 -0300

    feat: add customer ratings filter and sorting options to search page

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,4 +1,5 @@
 import ProductCard from '@/components/shared/products/product-card'
+import { Button } from '@/components/ui/button'
 import { getAllProducts, getAllCategories } from '@/lib/actions/product.actions'
 import Link from 'next/link'
 
@@ -9,6 +10,9 @@ const prices = [
   { name: '201$ to 500$', value: '201-500' },
   { name: '501$ to 1000$', value: '501-1000' }
 ]
+
+const ratings = [4, 3, 2, 1]
+const sortOrders = ['newest', 'lowest', 'highest', 'rating']
 
 const SearchPage = async (props: {
   searchParams: Promise<{
@@ -126,8 +130,61 @@ const SearchPage = async (props: {
             ))}
           </ul>
         </div>
+        {/* Rating link */}
+        <div className="text-xl mb-2 mt-8">Customer Ratings</div>
+        <div>
+          <ul className="space-y-1">
+            <li>
+              <Link
+                className={`${rating === 'all' && 'font-bold'}`}
+                href={getFilterUrl({ r: 'all' })}
+              >
+                Any
+              </Link>
+            </li>
+            {ratings.map(r => (
+              <li key={r}>
+                <Link
+                  className={`${rating === r.toString() && 'font-bold'}`}
+                  href={getFilterUrl({ r: `${r}` })}
+                >
+                  {`${r} stars & up`}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
       <div className="md:col-span-4 space-y-4">
+        <div className="flex-between flex-col md:flex-row my-4">
+          <div className="flex items-center">
+            {q !== 'all' && q !== '' && 'Query: ' + q}
+            {category !== 'all' && category !== '' && 'Category: ' + category}
+            {price !== 'all' && ' Price: ' + price}
+            {rating !== 'all' && ' Rating: ' + rating + ' stars & up'}
+            &nbsp;
+            {(q !== 'all' && q !== '') ||
+            (category !== 'all' && category !== '') ||
+            rating !== 'all' ||
+            price !== 'all' ? (
+              <Button variant={'link'} asChild>
+                <Link href="/search">Clear</Link>
+              </Button>
+            ) : null}
+          </div>
+          <div>
+            Sort by{' '}
+            {sortOrders.map(s => (
+              <Link
+                key={s}
+                className={`mx-2 ${sort === s && 'font-bold'}`}
+                href={getFilterUrl({ s })}
+              >
+                {s}
+              </Link>
+            ))}
+          </div>
+        </div>
         <div className="grid grid-cols-1 gap-3 md:grid-cols-3">
           {products.data.length === 0 && <div>No products found</div>}
           {products.data.map(product => (
