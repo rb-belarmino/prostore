674a670f51fcb1eed89969f5958e3e353722d3e5
Rodrigo Belarmino <rb.belarmino@gmail.com>
2025-03-15 15:23:18 -0300

    feat: enhance review form and list with review retrieval and reload functionality

--- app/(root)/product/[slug]/review-list.tsx
+++ app/(root)/product/[slug]/review-list.tsx
@@ -36,9 +36,12 @@ const ReviewList = ({
     loadReviews()
   }, [productId])
 
-  const reload = () => {
-    console.log('Review Submitted')
+  // Reload reviews after created or updated
+  const reload = async () => {
+    const res = await getReviews({ productId })
+    setReviews([...res.data])
   }
+
   return (
     <div className="space-y-4">
       {reviews.length === 0 && <div>No reviews yet</div>}
