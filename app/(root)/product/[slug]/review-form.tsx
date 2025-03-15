7337e911d13f7856fd2993dec8653250d16d8332
Rodrigo Belarmino <rb.belarmino@gmail.com>
2025-03-15 14:32:03 -0300

    feat: enhance review form with submission handling and reload functionality

--- app/(root)/product/[slug]/review-form.tsx
+++ app/(root)/product/[slug]/review-form.tsx
@@ -32,8 +32,9 @@ import { insertReviewSchema } from '@/lib/validators'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { StarIcon } from 'lucide-react'
 import { useState } from 'react'
-import { useForm } from 'react-hook-form'
+import { SubmitHandler, useForm } from 'react-hook-form'
 import { z } from 'zod'
+import { createUpdateReview } from '@/lib/actions/review.actions'
 
 const ReviewForm = ({
   userId,
@@ -42,7 +43,7 @@ const ReviewForm = ({
 }: {
   userId: string
   productId: string
-  onReviewSubmitted?: () => void
+  onReviewSubmitted: () => void
 }) => {
   const [open, setOpen] = useState(false)
 
@@ -53,8 +54,37 @@ const ReviewForm = ({
     defaultValues: reviewFormDefaultValues
   })
 
+  // Open form handler
   const handleOpenForm = () => {
+    form.setValue('productId', productId)
+    form.setValue('userId', userId)
+
     setOpen(true)
+  }
+
+  // Submit form handler
+  const onSubmit: SubmitHandler<
+    z.infer<typeof insertReviewSchema>
+  > = async value => {
+    const res = await createUpdateReview({
+      ...value,
+      productId
+    })
+
+    if (!res.success) {
+      return toast({
+        variant: 'destructive',
+        description: res.message
+      })
+    }
+
+    setOpen(false)
+
+    onReviewSubmitted()
+
+    toast({
+      description: res.message
+    })
   }
 
   return (
@@ -64,7 +94,7 @@ const ReviewForm = ({
       </Button>
       <DialogContent className="sm:max-w-[425px]">
         <Form {...form}>
-          <form method="POST">
+          <form method="POST" onSubmit={form.handleSubmit(onSubmit)}>
             <DialogHeader>
               <DialogTitle>Write a Review</DialogTitle>
               <DialogDescription>
